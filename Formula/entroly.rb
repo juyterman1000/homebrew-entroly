e407c806e824f08d6c12ac10f6f3269a46a1255f
juyterman1000 <fastrunner10090@gmail.com>
2026-06-29 21:34:20 -0700

    Bump Entroly to 1.0.40

--- Formula/entroly.rb
+++ Formula/entroly.rb
@@ -21,8 +21,8 @@ class Entroly < Formula
 
   desc "Token-saving proxy and context compression engine for AI coding agents"
   homepage "https://github.com/juyterman1000/entroly"
-  url "https://files.pythonhosted.org/packages/source/e/entroly/entroly-1.0.11.tar.gz"
-  sha256 "bb0bc104c65f8f31c832eae48c4d4749871431dd43e9d6830f6fe494a96cf27f"
+  url "https://files.pythonhosted.org/packages/6b/c5/2538038fa27277162690e9388963e0e3b98ada14773fd63ca17ab964f71d/entroly-1.0.40.tar.gz"
+  sha256 "e682a698f8c23e8a155d543b82c02fb1057e6ac8f6b39d9d1bdda71cea82989a"
   license "Apache-2.0"
   head "https://github.com/juyterman1000/entroly.git", branch: "main"
 
